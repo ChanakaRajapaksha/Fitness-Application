d43d4fe986cf14faaeac219be327f916ce68d700
RCP Rajapaksha <it20137250@my.sliit.lk>
2023-01-20 23:41:48 +0530

    create homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -25,6 +26,7 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
