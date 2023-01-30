41555ea80d54568e249a5ccc14e1822c8f399875
RCP Rajapaksha <it20137250@my.sliit.lk>
2023-01-30 20:34:57 +0530

    create contactus page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
 import OurClasses from "./scenes/ourclasses";
 import ContactUs from "@/scenes/contactUs";
+import Footer from "@/scenes/footer";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -33,6 +34,7 @@ function App() {
       <Benefits setSelectedPage={setSelectedPage} />
       <OurClasses setSelectedPage={setSelectedPage} />
       <ContactUs setSelectedPage={setSelectedPage} />
+      <Footer />
     </div>
   );
 }
