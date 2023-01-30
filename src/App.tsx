91c30d54f1511b81d084158bb56ff117de9065bb
RCP Rajapaksha <it20137250@my.sliit.lk>
2023-01-30 14:47:38 +0530

    create benefits page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/scenes/navbar";
 import Home from "@/scenes/home";
+import Benefits from "@/scenes/benefits";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
 
@@ -27,6 +28,7 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
   );
 }
--- src/shared/types.ts
+++ src/shared/types.ts
@@ -4,3 +4,9 @@ export enum SelectedPage {
   OurClasses = "ourClasses",
   ContactUs = "contactus",
 }
+
+export interface BenefitType {
+  icon: JSX.Element;
+  title: string;
+  description: string;
+}
